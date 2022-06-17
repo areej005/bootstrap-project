8ce7fca7289a5c9b6ba6d7c053cec71f3e4addda
areej <areej.bcs005@gmail.com>
2022-06-17 00:46:24 -0700

    Add project files.

---
+++ bootstrap project/JavaScript.js
@@ -0,0 +1,19 @@
+﻿ 
+        var swiper = new Swiper(".home-slider", {
+            effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
+            },
+            loop: true,
+            autoplay: {
+                delay: 4000,
+                disableOninterectaction: false,
+            }
+        });
